5b84cf43d99391e1f88357a5924db8613b90b59a
Dann-13 <dannyelgomezz.13@gmail.com>
2023-03-16 20:28:56 -0500

    Mostrar listas de Tareas

---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id:0,
+        title:"Mi primer Tarea",
+        description:"Mi Primer Tarea"
+    },
+    {
+        id:1,
+        title:"Mi segunda Tarea",
+        description:"Mi Primer Tarea"
+    },
+    {
+        id:2,
+        title:"Mi tercera Tarea",
+        description:"Mi Primer Tarea"
+    }
+]
